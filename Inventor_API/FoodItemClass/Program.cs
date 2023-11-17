d3488632becf1e2147491ff5c21e7c44ecd86bb7
Kachi G. Wachikwu <123011868+Wkachi@users.noreply.github.com>
2023-11-17 16:25:23 +0000

    FoodItemClass

---
+++ Inventor_API/FoodItemClass/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FoodItemClass;
+
+Console.WriteLine("Hello, World!");
+
+var Display = new FoodItem();
+Display.Name = "Rice";
+Display.Description = "Grains";
+Display.Price = 100.00;
+
+
---
+++ Inventor_API/FoodItemDB/Mappings/FoodItemClassMap.cs
@@ -0,0 +1,22 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodItemClass;
+
+namespace FoodItemDB.Mappings
+{
+    public class FoodItemClassMap : ClassMap<FoodItem>
+    {
+        public FoodItemClassMap() 
+        {
+            Id(food => food.Id);
+            HasMany(food => food.Name);
+            HasMany(food => food.Description);
+            References(food => food.Price);
+
+        }
+    }
+}
