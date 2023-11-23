963a4249a6217dfee54c2809dc93a40ebd36ab6f
Kachi G. Wachikwu <123011868+Wkachi@users.noreply.github.com>
2023-11-23 13:32:38 +0000

    deleted fluentNhibernate and sqlClient dependencies and added the virtual keyword

--- Inventor_API/Mappings/FoodItemMap.cs
+++ Inventor_API/Mappings/FoodItemMap.cs
@@ -1,6 +1,17 @@
-﻿namespace Inventor_API.Mappings
+﻿using FluentNHibernate.Mapping;
+using Inventor_API.Models;
+
+namespace Inventor_API.Mappings
 {
-    public class FoodItemMap
+    public class FoodItemMap : ClassMap<FoodItem>
     {
+        public FoodItemMap()
+        {
+            Id(food => food.Id);
+            Map(food => food.Name);
+            Map(food => food.Description);
+
+
+        }
     }
 }
