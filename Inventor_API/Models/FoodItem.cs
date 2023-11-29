9a14a79360604127e5f742cd7dd4a3e18c2f8057
David Ohiosimuan Ohimai <daveohimai@gmail.com>
2023-11-29 11:23:08 +0100

    Update FoodItem.cs
    Edit food item file

--- Inventor_API/Models/FoodItem.cs
+++ Inventor_API/Models/FoodItem.cs
@@ -2,23 +2,6 @@
 {
     public class FoodItem
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-
-
-        public FoodItem(string name, string description, double price)
-        {
-
-            Name = name;
-            Description = description;
-
-
-
-        }
-
-        public FoodItem()
-        {
-        }
+        
     }
 }
